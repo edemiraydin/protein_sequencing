819b9d954ce1b869b84cc0d0a8c179c79c57431c
Eda Johnson <edemiraydin@users.noreply.github.com>
2023-12-05 18:21:27 -0600

    Update setup.sql

--- src/sql/setup.sql
+++ src/sql/setup.sql
@@ -17,15 +17,3 @@ max_nodes=1;
 
 create image repository test_db.public.images;
 
-DROP SERVICE streamlit;
-CREATE SERVICE streamlit
-  MIN_INSTANCES = 1
-  MAX_INSTANCES = 1
-  COMPUTE_POOL = streamlit_pool
-  SPEC = '@yaml_stage/spec.yaml';
-
-ls @test_db.public.llm_workspace;
-
-CALL SYSTEM$GET_SERVICE_STATUS('TEST_DB.PUBLIC.STREAMLIT', 100); 
-DESCRIBE SERVICE STREAMLIT; -- get the endpoint URL for the streamlit app
-CALL SYSTEM$GET_SERVICE_LOGS('TEST_DB.PUBLIC.STREAMLIT', '0', 'streamlit');
